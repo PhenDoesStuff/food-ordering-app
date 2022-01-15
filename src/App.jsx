6048e59a0679fa5ea9f8ace5ab3227306cb35ef0
Stephen Montague <me@montaguestephen.com>
2022-01-15 17:30:26 -0500

    Creation of the header

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,11 @@
 import './App.css';
+import Header from './components/Layout/Header';
 
 function App() {
 	return (
-		<div className='App'>
-			<h2>Let's get started!</h2>
-		</div>
+		<>
+			<Header />
+		</>
 	);
 }
 
--- src/components/Layout/Header.jsx
+++ src/components/Layout/Header.jsx
@@ -1,7 +1,23 @@
 import React from 'react';
 
+import mealsImage from '../../Assets/meals.jpg';
+import './Header.css';
+
 function Header() {
-	return <div className='header'></div>;
+	return (
+		<>
+			<header className='header'>
+				<h1>ReactMeals</h1>
+				<button>Cart</button>
+			</header>
+			<div className='main-image'>
+				<img
+					src={mealsImage}
+					alt='A table full of delicious food!'
+				/>
+			</div>
+		</>
+	);
 }
 
 export default Header;
