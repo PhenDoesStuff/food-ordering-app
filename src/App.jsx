e3930d63e05222b5e91321325242fad00e5e6e4f
Stephen Montague <me@montaguestephen.com>
2022-02-13 16:46:04 -0500

    Starting over

--- src/App.jsx
+++ src/App.jsx
@@ -1,30 +1,7 @@
-import { useState } from 'react';
 import './App.css';
-import Cart from './components/Cart/Cart';
-import Header from './components/Layout/Header/Header';
-import Meals from './components/Meals/Meals';
-import CartProvider from './store/CartProvider';
 
 function App() {
-	const [cartIsShow, setCartIsShown] = useState(false);
-
-	const showCartHandler = () => {
-		setCartIsShown(true);
-	};
-
-	const hideCartHandler = () => {
-		setCartIsShown(false);
-	};
-
-	return (
-		<CartProvider>
-			{cartIsShow && <Cart onClose={hideCartHandler} />}
-			<Header onShowCart={showCartHandler} />
-			<main>
-				<Meals />
-			</main>
-		</CartProvider>
-	);
+	return <h1>Place holder</h1>;
 }
 
 export default App;
