3122230c23cc7fb856ccc4a7de21961cbb088ff3
Stephen Montague <stephen.d.montague@gmail.com>
2022-03-04 22:07:29 -0500

    Creation of a nice header cart button.

--- src/components/layout/Header.jsx
+++ src/components/layout/Header.jsx
@@ -1,13 +1,14 @@
 import React from 'react';
 import './Header.css';
 import meals from '../../static/images/meals.jpg';
+import HeaderCartButton from './HeaderCartButton';
 
 const Header = () => {
 	return (
 		<>
 			<header className='header'>
 				<h1>ReactMeals</h1>
-				<button>Cart</button>
+				<HeaderCartButton />
 			</header>
 			<div className='main-image'>
 				<img src={meals} alt='Header image' />
