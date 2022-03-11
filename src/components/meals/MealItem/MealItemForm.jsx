d715a62953e3bda4e503b935b8705e4af2624a86
Stephen Montague <stephen.d.montague@gmail.com>
2022-03-10 21:32:33 -0500

    Moving to laptop.

---
+++ src/components/meals/MealItem/MealItemForm.jsx
@@ -0,0 +1,13 @@
+import React from 'react'
+import './MealItemForm.css'
+
+const MealItemForm = () => {
+  return (
+    <form className='form'>
+      <input type="text" name="" id="" />
+      <button>Add</button>
+    </form>
+  );
+}
+
+export default MealItemForm
