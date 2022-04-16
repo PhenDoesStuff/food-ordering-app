b8e209f1a90e4a6bc5e3ef999e4e7f2f00b68800
Stephen Montague <Stephen.D.Montague@gmail.com>
2022-04-16 00:20:29 -0400

    Starting the lecture section over and using CSS Modules.

--- src/App.jsx
+++ src/App.jsx
@@ -1,32 +1,7 @@
 import './App.css';
-import Header from './components/layout/Header';
-import Meals from './components/meals/Meals';
-import Cart from './components/cart/Cart';
-import { useState } from 'react';
-import CartProvider from './store/CartProvider';
 
 function App() {
-	const [cartIsShown, setCartIsShown] = useState(false);
-
-	const showCartHandler = () => {
-		setCartIsShown(true);
-	};
-
-	const hideCartHandler = () => {
-		setCartIsShown(false);
-	};
-
-	return (
-		<CartProvider>
-			{cartIsShown && (
-				<Cart onClose={hideCartHandler} />
-			)}
-			<Header onShowCart={showCartHandler} />
-			<main>
-				<Meals />
-			</main>
-		</CartProvider>
-	);
+	return <h1>hodl</h1>;
 }
 
 export default App;
