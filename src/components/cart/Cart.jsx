f0c623016117462770fe0a322e26e2b8f360b67a
Stephen Montague <Stephen.D.Montague@gmail.com>
2022-04-15 23:57:34 -0400

    Working on outputting the cart items. CSS is now broken with these files in place. Asked question on forum about.

--- src/components/cart/Cart.jsx
+++ src/components/cart/Cart.jsx
@@ -1,19 +1,31 @@
-import React from 'react';
-import './Cart.css';
+import { useContext } from 'react';
+
 import Modal from '../ui/Modal';
+import CartItem from './CartItem';
+import './Cart.css';
+import CartContext from '../../store/cart-context';
 
 const Cart = ({ onClose }) => {
+	const cartCtx = useContext(CartContext);
+
+	const totalAmount = `$${cartCtx.totalAmount.toFixed(2)}`;
+	const hasItems = cartCtx.items.length > 0;
+
+	const cartItemRemoveHandler = id => {};
+
+	const cartItemAddHandler = item => {};
+
 	const cartItems = (
 		<ul className='cart-items'>
-			{[
-				{
-					id: 'c1',
-					name: 'Sushi',
-					amount: 2,
-					price: 12.99,
-				},
-			].map(item => (
-				<li>{item.name}</li>
+			{cartCtx.items.map(item => (
+				<CartItem
+					key={item.id}
+					name={item.name}
+					amount={item.amount}
+					price={item.price}
+					onRemove={cartItemRemoveHandler.bind(null, item.id)}
+					onAdd={cartItemAddHandler.bind(null, item)}
+				/>
 			))}
 		</ul>
 	);
@@ -23,15 +35,13 @@ const Cart = ({ onClose }) => {
 			{cartItems}
 			<div className='total'>
 				<span>Total Amount</span>
-				<span>35.62</span>
+				<span>{totalAmount}</span>
 			</div>
 			<div className='actions'>
-				<button className='button--alt'>
+				<button className='button--alt' onClick={onClose}>
 					Close
 				</button>
-				<button className='button'>
-					Order
-				</button>
+				{hasItems && <button className='button'>Order</button>}
 			</div>
 		</Modal>
 	);
