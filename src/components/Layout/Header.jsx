04cf8eb0c0b98c414d8aab74045acfb03f0ca146
Stephen Montague <Stephen.D.Montague@gmail.com>
2022-04-18 06:27:09 -0400

    Using prop drilling from App.js to be able to close the modal overlay that we created for the cart.

--- src/components/Layout/Header.jsx
+++ src/components/Layout/Header.jsx
@@ -2,12 +2,12 @@ import classes from './Header.module.css';
 import image from '../../assets/index.jpeg';
 import HeaderCartButton from './HeaderCartButton';
 
-const Header = () => {
+const Header = props => {
 	return (
 		<>
 			<header className={classes.header}>
 				<h1>ReactMeals</h1>
-				<HeaderCartButton />
+				<HeaderCartButton onClick={props.onCartToggle} />
 			</header>
 			<div className={classes['main-image']}>
 				<img src={image} alt='A big tasty buffet' />
